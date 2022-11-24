3aa1e048d67593d5fa2f2499762565ffb52fc2f4
Trishan <tgunness@gmail.com>
2022-11-24 17:36:15 -0500

    add new job

---
+++ jobs/TrishanFeedTest.groovy
@@ -0,0 +1,46 @@
+job('TrishanFeedTest') {
+  description 'SonarQube analysis'
+  properties {
+        githubProjectUrl 'https://github.com/CareerCruising/Xello.Feed.API/'
+  }
+  logRotator {
+    numToKeep 20
+  }
+  label 'linux-docker'
+  concurrentBuild false
+  scm {
+    git {
+      remote {
+        url 'git@github.com:CareerCruising/Xello.Feed.API.git'
+        credentials 'github_ccbuilds'
+      }
+      branch 'refs/heads/main'
+    }      
+  }
+  triggers {
+    githubPush()
+  }
+  wrappers {
+    credentialsBinding {
+        string('GITHUB_TOKEN', 'github_ccbuilds')
+        string('SONAR_TOKEN', 'c9609518-8875-48a3-8069-6b92013d7d1d')
+    }
+    timeout {
+      absolute(15)
+    }
+  }
+  steps {
+    shell('''
+    #!/bin/bash -ex
+    PROJECT_NAME="Xello.Feed.API"
+    SONAR_PROJECT_KEY="Xello.Feed.API"
+    DOCKER_TEMPLATE="dockerfile_netsdk60_sonarqube_jenkins_ci"
+
+    IMG_NAME=$(echo "${PROJECT_NAME}.Sonar" | tr '[:upper:]' '[:lower:]')
+
+    curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
+    "https://raw.githubusercontent.com/CareerCruising/docker/master/templates/${DOCKER_TEMPLATE}" > ${DOCKER_TEMPLATE}
+
+    docker build -t ${IMG_NAME}:${BUILD_NUMBER} -f ./${DOCKER_TEMPLATE} --build-arg SONAR_URL=${SONAR_URL} --build-arg SONAR_TOKEN=${SONAR_TOKEN} --build-arg SONAR_PROJECT_KEY=${SONAR_PROJECT_KEY} --build-arg PROJECT_NAME=${PROJECT_NAME} .'''.stripIndent().trim())
+  }
+}
