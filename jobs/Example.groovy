6c9c60f431244cdd8d2822d11cd71df808ed0bbd
Trishan <tgunness@gmail.com>
2022-11-24 17:21:49 -0500

    .

--- jobs/Example.groovy
+++ jobs/Example.groovy
@@ -30,19 +30,23 @@ job('Example') {
     }
   }
   steps {
-    shell('''#!/bin/bash -ex
+    shell('''
+    #!/bin/bash -ex
     PROJECT_NAME="Xello.Feed.API"
     SONAR_PROJECT_KEY="Xello.Feed.API"
     DOCKER_TEMPLATE="dockerfile_netsdk60_sonarqube_jenkins_ci"
+
     IMG_NAME=$(echo "${PROJECT_NAME}.Sonar" | tr '[:upper:]' '[:lower:]')
+    
     curl -s -H "Authorization: token ${GITHUB_TOKEN}" \
     "https://raw.githubusercontent.com/CareerCruising/docker/master/templates/${DOCKER_TEMPLATE}" > ${DOCKER_TEMPLATE}
+
     docker build \
     -t ${IMG_NAME}:${BUILD_NUMBER} \
     -f ./${DOCKER_TEMPLATE} \
     --build-arg SONAR_URL=${SONAR_URL} \
     --build-arg SONAR_TOKEN=${SONAR_TOKEN} \
     --build-arg SONAR_PROJECT_KEY=${SONAR_PROJECT_KEY} \
-    --build-arg PROJECT_NAME=${PROJECT_NAME} .''')
+    --build-arg PROJECT_NAME=${PROJECT_NAME} .'''.stripIndent().trim())
   }
 }
